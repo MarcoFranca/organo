9f8915c3b32a805c5b62473bcdf084543c166a65
Marco Franca <dev.marcotullio@gmail.com>
2022-09-10 20:22:21 -0300

    capture form values

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Banner from "./components/Banner";
 import {Form} from "./components/Form";
+import {useState} from "react";
 
 function App() {
+
+    const [contributers, setContributer] = useState([])
+
+    const toNewContributer = (contributer) =>{
+        setContributer([...contributers,contributer])
+        console.log(contributer)
+    }
+
     return (
         <div className="App">
             <Banner/>
-            <Form/>
+            <Form
+                registeredContributor = {contributer => toNewContributer(contributer)}
+            />
         </div>
     );
 }
