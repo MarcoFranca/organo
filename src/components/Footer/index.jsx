078e30991e90631f9a11affbe1dbd348254c072a
Marco Franca <dev.marcotullio@gmail.com>
2022-09-12 18:29:21 -0300

    change the url

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,8 +4,12 @@ export default function Footer () {
     return (
         <div className='footer'>
             <div className='footer__media'>
-                {/*<a href="https://www.facebook.com/" target="_blank"><img src="./image/fb.png" alt="face"/></a>*/}
-                {/*<a href="#" target="_blank"><img src="./image/tw.png" alt="tweeter"/></a>*/}
+                <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+                    <img src="./image/fb.png" alt="face"/>
+                </a>
+                <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+                    <img src="./image/tw.png" alt="tweeter"/>
+                </a>
                 {<a href="https://twitter.com/MarcotullioDev" target="_blank" rel="noreferrer">
                     <img src="./image/ig.png" alt="instagram"/>
                 </a>}
