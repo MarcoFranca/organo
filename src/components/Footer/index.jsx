e1860d0dc2d512cbaeda367e45363db2d598a689
Marco Franca <dev.marcotullio@gmail.com>
2022-09-12 15:54:28 -0300

    footer template

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,10 +6,10 @@ export default function () {
             <div className='footer__media'>
                 <a href="#"><img src="./image/footer/fb.png" alt="face"/></a>
                 <a href="#"><img src="./image/footer/tw.png" alt="tweeter"/></a>
-                <a href="#"><img src="./image/footer/ig.png" alt=""/></a>
+                <a href="#"><img src="./image/footer/ig.png" alt="instagram"/></a>
             </div>
             <div>
-                <img src="./image/footer/logo.png" alt=""/>
+                <img src="./image/footer/logo.png" alt="organo"/>
             </div>
             <div>
                 <p>Desenvolvido por Alura & Marco Tullio</p>
