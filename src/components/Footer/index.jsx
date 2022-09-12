eb20ad123208138035a1aca121288e82e5c06b52
Marco Franca <dev.marcotullio@gmail.com>
2022-09-12 16:06:30 -0300

    change the url

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,12 +4,12 @@ export default function Footer () {
     return (
         <div className='footer'>
             <div className='footer__media'>
-                <a href="#"><img src="./image/footer/fb.png" alt="face"/></a>
-                <a href="#"><img src="./image/footer/tw.png" alt="tweeter"/></a>
-                <a href="#"><img src="./image/footer/ig.png" alt="instagram"/></a>
+                <a href="#"><img src="/image/footer/fb.png" alt="face"/></a>
+                <a href="#"><img src="/image/footer/tw.png" alt="tweeter"/></a>
+                <a href="#"><img src="/image/footer/ig.png" alt="instagram"/></a>
             </div>
             <div>
-                <img src="./image/footer/logo.png" alt="organo"/>
+                <img src="/image/footer/logo.png" alt="organo"/>
             </div>
             <div>
                 <p>Desenvolvido por Alura & Marco Tullio</p>
